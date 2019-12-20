3d28c6dba1cf3788592fba1f1fce0c633d56efda
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-20 22:52:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -143,5 +143,23 @@ def big_shoe_rebounds()
 end
 
 def most_points_scored()
+
+  max = 0
+  playername = ""
+  game_hash.each do |key, value|
+    value.each do|name_color_players, values|
+      if(name_color_players == :players)
+        values.each do|player_hash|
+          if(player_hash[:shoe]>maxshoesize)
+              max = player_hash[:shoe]
+              playername = player_hash[:player_name]
+          end
+        end
+      end
+    end
+  end
+  
+
+  return playername
   
 end
