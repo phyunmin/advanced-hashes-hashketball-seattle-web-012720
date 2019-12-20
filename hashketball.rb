baf3d32934fb6c091bc5bb5cba9ac1db323a8a03
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-20 22:02:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -62,9 +62,7 @@ def team_colors(teamname)
   teamcolors = []
   game_hash.each do |key, value|
     if(value[:team_name] == teamname)
-      pp value[:team_name]
-      pp value[:colors]
-      team_colors = value[:team_colors]
+      team_colors = value[:colors]
     end
   end
   return teamcolors
